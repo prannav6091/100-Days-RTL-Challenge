48cf095a82a2aec8ef80d9d74e15712e2c7b3a90
prannav6091 <prannavrajvs@gmail.com>
2026-02-19 13:06:36 +0530

    Create 8_bit_alu_tb.sv

---
+++ day4/8_bit_alu_tb.sv
@@ -0,0 +1,25 @@
+module alu_tb();
+
+logic [7:0] a_i;
+logic [7:0] b_i;
+logic [2:0] op_i;
+logic [7:0] alu_o;
+
+alu ALU (.*);
+
+initial begin
+for (int j=0; j<3; j++) begin
+for (int i=0; i<7; i++) begin
+a_i = $urandom_range(0, 8'hFF);
+b_i = $urandom_range(0,8'hFF);
+op_i = 3'(i);
+#5;
+end
+end
+end
+
+initial begin
+$dumpfile("alu.vcd");
+$dumpvars(0, alu_tb);
+end
+endmodule
